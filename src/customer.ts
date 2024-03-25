fc140230cc21b87aaff8f7cd4c7e944e8fc8ace4
Audrey Alcantara <audreyalcantara@Audreys-MBP.lan>
2024-03-25 10:39:09 -0400

    refactor: deconstructed createStatement method down and split up responsability

--- src/customer.ts
+++ src/customer.ts
@@ -4,7 +4,7 @@ import { Rental } from './rental';
 class Customer {
 	public name: string;
 	public rentals: Array<Rental>;
-    private moviePriceMap: { [key: number]: (days: number) => number } = {
+    private moviePriceMap: { [key: number]: (daysRented: number) => number } = {
         [REGULAR_MOVIE]: this.regularMovie,
         [NEW_RELEASE_MOVIE]: this.newReleaseMovie,
         [CHILDRENS_MOVIE]: this.childrensMovie,
@@ -28,14 +28,12 @@ class Customer {
           let thisAmount = 0;
 
       thisAmount = this.calculateCost(rental);
+      totalAmount += thisAmount;
       frequentRenterPoints = this.updateRenterPoints(rental, frequentRenterPoints);
-      //result = this.updateResults(rental, thisAmount, frequentRenterPoints, result, totalAmount);
-
-          result += "\t" + rental.getMovie().getTitle() + "\t" +
-                  thisAmount + "\n";
-          totalAmount += thisAmount;
-
+      result = this.appendResults(rental, thisAmount, result);
       }
+    //   let finalResult = this.finalResults(result,totalAmount,frequentRenterPoints);
+    //   finalResult = finalResult;
       result += "Amount owed is " + totalAmount + "\n";
       result += "You earned " + frequentRenterPoints +
               " frequent renter points";
@@ -48,42 +46,48 @@ class Customer {
   }
 
   private updateRenterPoints(rental: Rental, frequentRenterPoints: number){
-    frequentRenterPoints++
-    if ((rental.getMovie().getPriceCode() == NEW_RELEASE_MOVIE) &&
-        rental.getDaysRented() > 1) frequentRenterPoints++;
+    frequentRenterPoints++;
+    if ((rental.getMovie().getPriceCode() == NEW_RELEASE_MOVIE) && rental.getDaysRented() > 1) 
+        frequentRenterPoints++;
     return frequentRenterPoints;
   }
 
-  private updateResults(rental: Rental, thisAmount: number, frequentRenterPoints: number, result: string, totalAmount: number){
-    // result += "\t" + rental.getMovie().getTitle() + "\t" +
-    //               thisAmount + "\n";
-    //       totalAmount += thisAmount;
-    //     return result;
+  private appendResults(rental: Rental, thisAmount: number, result: string){
+    result += "\t" + rental.getMovie().getTitle() + "\t" + thisAmount + "\n";
+    return result;
   }
 
-  private regularMovie(days: number) {
+  private regularMovie(daysRented: number) {
     let amount = 2;
-    if (days > 2) {
-        amount += (days -2) * 1.5;
+    if (daysRented > 2) {
+        amount += (daysRented -2) * 1.5;
     }
     return amount;
 }
 
-private newReleaseMovie(days: number) {
-    return days * 3 
+private newReleaseMovie(daysRented: number) {
+    return daysRented * 3 
 }
 
-private childrensMovie(days: number) {
+private childrensMovie(daysRented: number) {
     let amount = 1.5;
-    if(days > 3){
-    amount += (days - 3) * 1.5;
+    if(daysRented > 3){
+    amount += (daysRented - 3) * 1.5;
     }
     return amount;
 }
 
-private priceCodeThree(days: number){
-    return days * 0
+private priceCodeThree(daysRented: number){
+    return daysRented * 0
 }
+
+
+//   private finalResults(result: string,totalAmount: number,frequentRenterPoints: number){
+//     result += "Amount owed is " + totalAmount + "\n";
+//       result += "You earned " + frequentRenterPoints +
+//               " frequent renter points";
+//       return result;
+//   }
 
 
 }
